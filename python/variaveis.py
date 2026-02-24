81914ee9898731a3b4c804e9467d3140ad079059
Luciano <lucbran11@gmail.com>
2026-02-24 18:48:26 -0300

    atualizacao com variavies

--- python/variaveis.py
+++ python/variaveis.py
@@ -1,12 +1,25 @@
-faturamento = 1000
+faturamento = 1000 # int
 custo = 600
 
 novas_vendas = 1100
 faturamento = faturamento + novas_vendas
-lucro = faturamento - custo
+
+imposto = 0.15 * faturamento # float   
+print (imposto)
+
+lucro = faturamento - custo - imposto
+
 
 
 print ("faturamento", faturamento)
 print ("custo", custo)
 print ("lucro", lucro)
 
+
+messagem = "O faturamento da loja foi de R$ 2100" # string
+teve lucro = True
+
+# int = numeros inteiros
+# float = numeros decimais
+# strings  = texto
+# boleanos = verdadeiro ou falso    
