841aefa756efcf0d9326e4d1ca5a18b8bbf10e0e
Luciano <lucbran11@gmail.com>
2026-02-24 18:29:34 -0300

    exercicio atualizado, variaveis criadas, arquivo novo criado

---
+++ python/variaveis.py
@@ -0,0 +1,12 @@
+faturamento = 1000
+custo = 600
+
+novas_vendas = 1100
+faturamento = faturamento + novas_vendas
+lucro = faturamento - custo
+
+
+print ("faturamento", faturamento)
+print ("custo", custo)
+print ("lucro", lucro)
+
