8332c01a28a6cec4393e55e02bd757bd3bf6ee35
Luciano <lucbran11@gmail.com>
2026-02-25 14:55:15 -0300

    autualização do arquivo variaveis.py

--- python/variaveis.py
+++ python/variaveis.py
@@ -17,9 +17,29 @@ print ("lucro", lucro)
 
 
 messagem = "O faturamento da loja foi de R$ 2100" # string
-teve lucro = True
+teve_lucro = True #boolean
+
+margem_lucro = lucro / faturamento * 100
+print ("margem", margem_lucro)
+
 
 # int = numeros inteiros
 # float = numeros decimais
 # strings  = texto
 # boleanos = verdadeiro ou falso    
+
+# operadores especiais
+
+# mod -> %
+# resto da divisão de um numero por outro
+# 10 % 3 
+
+anos = int (310/12) # 25
+print (anos, "anos")
+
+meses = 310 % 12 # 10
+print (meses, "meses")
+
+
+
+# floor division -> //
