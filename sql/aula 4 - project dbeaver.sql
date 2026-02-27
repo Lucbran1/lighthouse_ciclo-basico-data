14169e1be6d7f9b6923e34f8e510f33aa7df0c20
Luciano <lucbran11@gmail.com>
2026-02-26 22:09:42 -0300

    Sql - claude + github copilot coding agent to accelerate workflows

--- sql/aula 4 - project dbeaver.sql
+++ sql/aula 4 - project dbeaver.sql
@@ -1,1 +1,14 @@
+-- Active: 1772152469331@@127.0.0.1@3306
 
+-- 3 CATEGORIAS QUE MAIS GERARAM RECEITA TOTAL
+SELECT
+    c.CategoryName,
+    SUM(od.UnitPrice * od.Quantity) AS receita_total
+FROM Category c
+INNER JOIN Product p
+    ON c.Id = p.CategoryId
+INNER JOIN OrderDetail od
+    ON p.Id = od.ProductId
+GROUP BY c.CategoryName
+ORDER BY receita_total DESC
+LIMIT 3;
